ffef5f901040ddea657f6bc7e90bd7e027628779
GayathriBandi <153480381+GayathriBandi@users.noreply.github.com>
2025-07-15 12:01:09 +0530

    Create LC-Maximum Depth of Binary Tree.c

---
+++ Week-2/Day-11/LC-Maximum Depth of Binary Tree.c
@@ -0,0 +1,18 @@
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
